10ce758b8d2067dad7b071a5d93c89407f8d382e
DAPPERstats <dapperstats@gmail.com>
2017-12-11 18:35:00 -0800

    unintentional delete Revert "deleting old files"
    This reverts commit 41cfb05ce2872f3b5be5721b0a9fd3914afabae4.

---
+++ GenEstApp/ui.R
@@ -0,0 +1,411 @@
+##############################################################################
+#
+#  This script contains the UI code for the the GenEst app
+#
+#  version 0.0.0.5 November 2017
+#
+#  Held under GNU GPL v >= 3	
+#
+##############################################################################
+
+# main page
+
+  navbarPage("GenEst",
+
+    # Home tab: splash page            
+
+      tabPanel("Home",
+        br(),
+        HTML('<center><img src="GenEstLogoExample1.jpg" height = 
+                              "500"></center>'),
+        br()
+      ),
+
+    # Data Input tab: input and view each of the data tables
+
+      tabPanel("Data Input",
+
+          sidebarLayout(
+            sidebarPanel(width = 3,
+              HTML('<b><big><center>Upload data:</center></big></b>'),
+              br(),
+              fileInput("SEFile", "Choose Search Efficiency Data File",
+                accept = c(
+                "text/csv",
+                "text/comma-separated-values,text/plain",
+                ".csv")
+              ),
+              br(),
+              fileInput("CPFile", "Carcass Persistence Data File",
+                accept = c(
+                "text/csv",
+                "text/comma-separated-values,text/plain",
+                ".csv")
+              ),
+              br(),
+              fileInput("SSFile", "Search Schedule Data File",
+                accept = c(
+                "text/csv",
+                "text/comma-separated-values,text/plain",
+                ".csv")
+              ),
+              br(),
+              fileInput("COFile", "Carcass Observation Data File",
+                accept = c(
+                "text/csv",
+                "text/comma-separated-values,text/plain",
+                ".csv")
+              ),
+              br(),
+              fileInput("MDFile", "Meta Data File",
+                accept = c(
+                "text/csv",
+                "text/comma-separated-values,text/plain",
+                ".csv")
+              )
+            ),
+            mainPanel(
+              HTML('<b><big><center>View data:</center></big></b>'),
+              br(),
+              tabsetPanel(
+                tabPanel("Search Efficiency", br(), br(), 
+                          dataTableOutput("SEdata")),
+                tabPanel("Carcass Persistence", br(), br(), 
+                          dataTableOutput("CPin")),
+                tabPanel("Search Schedule", br(), br(), 
+                          dataTableOutput("SSin")),
+                tabPanel("Carcass Observations", br(), br(), 
+                          dataTableOutput("COin")),
+                tabPanel("Meta Data", br(), br(), 
+                          tableOutput("MDin"))
+              )
+            )
+          )
+      ),
+
+
+    # Analyses tab: set up and run the analyses 
+
+      tabPanel("Analyses",
+        tabsetPanel(
+
+          # General Inputs tab
+
+            tabPanel("General Inputs",
+              br(), br(),
+ 
+              # side bar of general inputs 
+ 
+                sidebarPanel(width = 3,
+                    numericInput("Niterations", "Number of iterations:", 
+                           value = 1000, min = 1, max = 10000, step = 1),
+                    numericInput("CL", "Confidence Level:", 
+                           value = 0.9, min = 0, max = 1, step = 0.001),
+                    numericInput("ffs", "Fraction of units or area surveyed",
+                           value = 1.0, min = 0, max = 1, step = 0.001)
+              ),
+
+              # main panel is empty
+
+                mainPanel(
+                  br()
+                )
+            ),
+
+          # Search Efficiency tab
+
+            tabPanel("Search Efficiency",
+              br(), br(), 
+
+              # side bar of model inputs
+
+                sidebarPanel(width = 3, 
+                           selectizeInput("SEobscols", 
+                               "Choose observation columns (in order):",
+                               c("No data input yet"), multiple = T),
+                           selectizeInput("SEsizeclasscol", 
+                               "Choose size class column:", 
+                               c("No data input yet"), multiple = T, 
+                               options = list(maxItems = 1)),
+                           selectizeInput("SEvars", 
+                               "Choose predictor variables (max: 2):", 
+                               c("No data input yet"), multiple = T, 
+                               options = list(maxItems = 2)),
+                           selectizeInput("fixKchoice", "Use fixed k?",
+                               c("NO", "YES"), multiple = F),
+                           numericInput("fixKvalchoice", 
+                               "Value for fixed k:", value = NULL, 
+                                min = 0, max = 1, step = 0.001),
+                           actionButton("SEmodrun", "Run SE Model")
+                ),
+
+              # main panel is a tabset panel
+
+                mainPanel(
+                  tabsetPanel(
+
+                    # Data tab shows data being modeled
+
+                      tabPanel("Data", br(), br(), 
+                               dataTableOutput("selected_SE")
+                      ),
+
+                    # Model Table tab shows AIC table for a given size class
+
+                      tabPanel("Model Table", br(), 
+                           selectizeInput("SEaicsizeclass", 
+                                      "Choose size class for AIC table:", 
+                                      "Model not yet run", multiple = F),
+                           br(), 
+                           actionButton("SEaictablerun", 
+                                        "Generate AIC Table"), 
+                           br(), br(), 
+                           dataTableOutput("SEaictable") 
+                      ),
+
+                    # Figure tab produces a figure for a given size class and 
+                    #  selected model
+
+                      tabPanel("Figure", br(),
+                           selectizeInput("SEfigsizeclass", 
+                                      "Choose size class for SE figure:", 
+                                      "Model not yet run", multiple = F ),
+                           selectizeInput("SEfigmodel", 
+                                      "Choose model for SE figure:", 
+                                      "Model not yet run", multiple = F ),
+                           br(), 
+                           actionButton("SEfigrun", "Generate SE Figure"), 
+                           br(), br(), 
+                           plotOutput("SEfig", width = "800px", 
+                                       height = "800px")
+                      ),
+
+                    # Model Selection tab is used to select a model for each
+                    #  size class. Model selection options can only be 
+                    #  populated once the model has been run
+
+                      tabPanel("Model Selection", 
+                           br(), 
+                           actionButton("SEmodOpsPop", "Populate Options"),
+                           br(), br(),
+                           htmlOutput("SEmodselectinputs")
+                      ) 
+                  )
+                )
+            ),
+
+
+          # Carcass Persistence tab
+
+            tabPanel("Carcass Persistence",
+              br(), br(), 
+
+              # side bar of model inputs
+
+                sidebarPanel(width = 3,
+                  selectizeInput("CPltp", 
+                     "Choose last time present observation column:",
+                     c("No data input yet"), multiple = T, 
+                     options = list(maxItems = 1)),
+                  selectizeInput("CPfta", 
+                     "Choose first time absent observation column:",
+                     c("No data input yet"), multiple = T, 
+                     options = list(maxItems = 1)),
+                  selectizeInput("CPsizeclasscol", 
+                     "Choose size class column:",
+                     c("No data input yet"), multiple = T, 
+                     options = list(maxItems = 1)),
+                  selectizeInput("CPvars", 
+                     "Choose predictor variables (max: 2):", 
+                     c("No data input yet"), multiple = T, 
+                     options = list(maxItems = 2)),
+                  actionButton("CPmodrun", "Run CP Model")
+                ),
+
+              # main panel is a tabset panel
+
+                mainPanel(
+                  tabsetPanel(
+
+                    # Data tab shows data being modeled
+ 
+                      tabPanel("Data", br(), br(), 
+                               dataTableOutput("selected_CP")
+                      ),
+
+                    # Model Table tab shows AIC table for a given size class
+
+                      tabPanel("Model Table",  br(),
+                           selectizeInput("CPaicsizeclass", 
+                                          "Choose size class for AIC table:", 
+                                          "Model not yet run", multiple = F ),
+                           br(), 
+                           actionButton("CPaictablerun", 
+                                        "Generate AIC Table"),
+                           br(), br(), 
+                           dataTableOutput("CPaictable") 
+                      ),
+
+                    # Figure tab produces a figure for a given size class and 
+                    #  selected model
+
+                      tabPanel("Figures",  br(),
+                           selectizeInput("CPfigsizeclass",
+                                      "Choose size class for CP figure:",
+                                      "Model not yet run", multiple = F ),
+                           selectizeInput("CPfigmodelcomplexity",
+                                     "Choose model complexity for CP figure:", 
+                                     "Model not yet run", multiple = F ),
+                           selectizeInput("CPfigdistemph",
+                                       "Choose distribution to emphasize:", 
+                                       "Model not yet run", multiple = F ),
+                           br(), 
+                           actionButton("CPfigrun", "Generate CP Figure"), 
+                           br(), br(), 
+                           plotOutput("CPfig", width = "800px", 
+                                   height = "1000px")
+                      ),
+
+                    # Model Selection tab is used to select a model for each
+                    #  size class. Model selection options can only be 
+                    #  populated once the model has been run
+
+                      tabPanel("Model Selection", br(), 
+                           actionButton("CPmodOpsPop","Populate Options"),
+                           br(), br(),  
+                           htmlOutput("CPmodselectinputs")
+                      )
+                  )
+                )
+            ),
+
+          # Detection Probability tab
+
+            tabPanel("Detection Probability",
+              br(), br(), 
+
+              # side bar of model run button and search schedule translation
+
+                sidebarPanel(width = 3,
+                       actionButton("grun", "Estimate Detection Probability"), 
+                       br(), br(), tableOutput("SStable")
+                ),
+
+              # main panel displays the detection probability table
+
+                mainPanel(
+                  br(), 
+                  dataTableOutput("gtable"),
+                  br()
+                )
+            ),
+
+##############################################################################
+          # Fatality Estimation tab
+
+            tabPanel("Fatality Estimation",
+              br(), br(), 
+
+              # side bar of model inputs
+
+                sidebarPanel(width = 3, 
+                  selectizeInput("COunitcol", "Choose unit column:",
+                         c("No data input yet"), multiple = T, 
+                         options = list(maxItems = 1)),
+                  selectizeInput("COdfcol", 
+                         "Choose date found column:",
+                         c("No data input yet"), multiple = T, 
+                         options = list(maxItems = 1)),
+                  selectizeInput("COsizeclasscol", 
+                         "Choose size class column:", 
+                         c("No data input yet"), multiple = T, 
+                         options = list(maxItems = 1)),
+                  selectizeInput("COsplitcol", 
+                         "Choose split column:", 
+                         c("No data input yet"), multiple = T, 
+                         options = list(maxItems = 1)),
+                  actionButton("Mrun", "Estimate Total Carcasses")
+                ),
+
+              # main panel shows output table and figure
+
+                mainPanel(
+                  tabsetPanel(
+
+                    # Data tab shows data being modeled
+ 
+                      tabPanel("Data",   
+                           br(), br(), dataTableOutput("selected_CO")
+
+                      ),
+
+                    # Table tab shows the final fatality estimation table
+
+                      tabPanel("Table",  
+                           br(), br(),
+                           tableOutput("Mhattab")
+                      ),
+
+                    # Figure tab shows the final fatality estimation figure
+
+                      tabPanel("Figure",  br(),
+                           br(), br(),
+                           plotOutput("Mhatfig", width = "800px", 
+                                      height = "800px")
+                      )
+                  )
+                )
+
+            )
+        )
+      ),
+
+          # About tab: details about GenEst
+
+            tabPanel("About",
+              fluidRow(
+                column(6, offset = 3,
+                       HTML('<img src = "GenEstLogoExample1.jpg" 
+                              height = "400">'),
+                       br(), 
+                       br(),
+                       HTML('<b>Authors:</b>  
+                         Daniel Dalthorp
+                           <a href = "http://www.USGS.gov">(USGS)</a>,
+                         Juniper Simonis
+                           <a href = 
+                           "http://www.dapperstats.com">(DAPPER Stats)</a>,
+                         Lisa Madsen
+                           <a href = "http://www.OSU.edu">(OSU)</a>,
+                         Paul Rabie 
+                           <a href = "http://www.west-inc.com">(WEST)</a>, 
+                         and
+                         Manuela Huso
+                           <a href = "http://www.USGS.gov">(USGS)</a>'),
+                       br(),
+                       br(),
+                       HTML('GenEst is a tool for estimating bird and bat 
+                             fatalities at renewable power facilities.'),
+                       br(),
+                       br(),
+                       HTML('GenEst is currently in development
+                             and should be considered provisional.'),
+                       br(),
+                       br(),
+                       HTML('This is version 0.0.0.3, October 2017.'),
+                       br(),
+                       br(),
+                       HTML('The development of GenEst is being supported by 
+                         BatConservation International, The US Bureau of Land
+                         Management, The US Geological Survey, WEST, and 
+                         Oregon State University.'),
+                       br(),
+                       br(),
+                       HTML('GenEst is provided under GNU GPL version 3 (and 
+                              any later versions).')
+                )
+              )
+            )
+  )
+
+
---
+++ gvec.R
@@ -0,0 +1,144 @@
+# gvec -- a function for calculating detection probability in a single class given search schedule
+#   and simulated columns of carcass persistence times and pk parameters
+#
+# ARGUMENTS
+#   days = vector of search times, beginning at 0
+#   CPab = simulated vector of persistence parameters (Weibull if 2-d or exponential if 1-d)
+#     parameterization: for exponential? means; for Weibull? R's shape and scale (like eoa)
+#   seef = searcher efficiency parameters
+#     if k not derived from a pk model of field trial data, then seef is a vector of simulated
+#       searcher efficiencies and k is a constant defined by the user
+#     if k is derived from field trial data, then seef is an nsim x 2 array of simulated p and k
+#
+# VALUE
+#   A vector of simulated detection probabilities
+
+gvec <- function(days, CPab, persdist, seef, k = NULL){
+# need: error-checking (including k must be scalar between 0 and 1 if seef is a vector)
+  nsim <- dim(CPab)[1]
+  samtype <- ifelse(length(unique(diff(days))) == 1, "Formula", "Custom")
+  nsearch <- length(days) - 1
+  if (persdist %in% c("Exponential", "exponential")){
+    pdb <- CPab
+    pda <- 1/pdb
+    pdb0 <- exp(mean(log(pdb)))
+    pda0 <- 1/pdb0
+  } else {
+    pda <- CPab[, 1]
+    pdb <- CPab[, 2]
+    pdb0 <- exp(mean(log(pdb)))
+    pda0 <- 1/mean(1/pda)
+  }
+  pk <- array(dim = c(nsim, 2))
+  if (is.vector(seef)){
+    pk[, 1] <- seef
+    pk[, 2] <- k
+  } else {
+    pk <- seef
+  }
+
+  ###1. setting estimation control parameters
+  ##  a. search limit: number of searches after arrival to include in estimate of seef
+  ##   [when the number of searches is high, including them all in the estimation is
+  ##    calculation intensive but does not contribute signficantly to the result]
+  f0 <- mean(pk[, 1])
+  k0 <- mean(pk[, 2])
+  ind1 <- rep(1:nsearch, times = nsearch:1)
+  ind2 <- ind1+1
+  ind3 <- unlist(lapply(1:nsearch, function(x) x:nsearch)) + 1
+  schedule.index <- cbind(ind1,ind2,ind3)
+  schedule <- cbind(days[ind1],days[ind2],days[ind3])
+  nmiss <- schedule.index[,3] - schedule.index[,2]
+  maxmiss <- max(nmiss)
+  powk <- cumprod(c(1, rep(k0, maxmiss))) # vector of k^i's
+  notfind <- cumprod(1 - f0*powk[-length(powk)])
+  nvec <- c(1, notfind) * f0
+  # conditional probability of finding a carcass on the ith search (row) after arrival
+  # for given (simulated) searcher efficiency (column):
+  pfind.si <- nvec * powk
+  # persistences:
+  intxsearch <- unique(cbind(schedule[,2] - schedule[,1], schedule[,3] - schedule[,2]), MAR = 1)
+  ppersu <- ppersist(persdist,
+    t_arrive0 = 0,
+    t_arrive1 = intxsearch[,1],
+    t_search = intxsearch[,1] + intxsearch[,2],
+    pda = pda0, pdb = pdb0
+  )
+  arrvec <- (schedule[,2] - schedule[,1])/max(days)
+  prob_obs <- numeric(dim(schedule)[1])
+  for (i in 1:length(prob_obs)){
+    prob_obs[i] <- pfind.si[nmiss[i]+1] *
+      ppersu[which(
+        abs(intxsearch[,1] - (schedule[i,2] - schedule[i,1])) < 0.001 &
+        abs(intxsearch[,2] - (schedule[i,3] - schedule[i,2])) < 0.001
+      ),] * arrvec[i]
+  }
+  ggnm <- numeric(maxmiss+1)
+  for (missi in 0:maxmiss){
+    ggnm[missi+1] <- sum(prob_obs[nmiss==missi])
+  }
+  # if more than 10 searches, consider truncating search schedule because very few carcasses
+  # will be found after being missed 9 or more times, but very large number of searches is
+  # costly in terms of calculation efficiency
+  if (nsearch > 10){
+    iskip <- min(which(cumsum(ggnm)/sum(ggnm) > 0.99)) + 1
+    # cutting off the search schedule introduces a slight bias.
+    # Correct by multiplying the final g's by gadj = sum(ggnm)/ggnm[iskip]
+    gadj <- sum(ggnm)/sum(ggnm[1:iskip])
+  } else {
+    iskip <- maxmiss
+    gadj <- 1
+  }
+  ###2. estimation of g
+  #a. subset the search schedule
+  #   (ignoring probabilities of detection carcasses after they have been missed several times):
+  schedule <- cbind(days[ind1], days[ind2], days[ind3])[ind2 >= ind3 - iskip + 1,]
+  #columns for arrival interval and search number:
+  schedule.index <- cbind(ind1, ind2, ind3)[ind2 >= ind3 - iskip+1,]
+  nmiss <- schedule.index[,3] - schedule.index[,2]
+  maxmiss <- max(nmiss)
+  # searcher efficiencies
+  if (maxmiss == 0) {
+    pfind.si <- pk[,1]
+  } else if (maxmiss == 1){
+    pfind.si<-cbind(pk[,1], (1 - pk[,1]) * pk[,2]*pk[,1])
+  } else {
+    powk<-array(rep(pk[, 2], maxmiss + 1), dim=c(nsim, maxmiss+1))
+    powk[,1] <- 1
+    powk <- matrixStats::rowCumprods(powk)
+    pfind.si <- pk[,1] * powk *
+      cbind(rep(1, nsim), matrixStats::rowCumprods(1 - (pk[,1] * powk[, 1:maxmiss])))
+  }
+  intxsearch<-unique(cbind(schedule[,2] - schedule[,1], schedule[,3] - schedule[,2]), MAR=1)
+  ppersu<-ppersist(persdist,
+    t_arrive0 = 0,
+    t_arrive1 = intxsearch[,1],
+    t_search = intxsearch[,1] + intxsearch[,2],
+    pda = CPab[,1], pdb = CPab[,2]
+  )
+  # arrivals
+  arrvec <- (schedule[,2]-schedule[,1])/max(days) # assumes uniform arrivals
+  # add the probabilities
+  prob_obs <- numeric(nsim)
+  if (maxmiss > 0){
+    for (i in 1:dim(schedule)[1]){
+      prob_obs <- prob_obs +
+        pfind.si[,nmiss[i]+1] *
+        ppersu[which(
+          abs(intxsearch[,1] - (schedule[i,2] - schedule[i,1])) < 0.001 &
+          abs(intxsearch[,2] - (schedule[i,3] - schedule[i,2])) < 0.001),
+        ] * arrvec[i]
+    }
+  } else {
+    for (i in 1:dim(schedule)[1]){
+      prob_obs <- prob_obs +
+      pfind.si[nmiss[i]+1] *
+      ppersu[which(
+        abs(intxsearch[,1] - (schedule[i,2] - schedule[i,1])) < 0.001 &
+        abs(intxsearch[,2] - (schedule[i,3] - schedule[i,2])) < 0.001),
+      ] * arrvec[i]
+    }
+  }
+  # g for monitored period
+  prob_obs
+}
