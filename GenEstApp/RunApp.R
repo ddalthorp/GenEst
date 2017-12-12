9cf90b92e78c649bc23a937a1cbd4e920f8d6044
DAPPERstats <dapperstats@gmail.com>
2017-12-11 18:35:00 -0800

    unintentional delete Revert "deleting old files"
    This reverts commit 41cfb05ce2872f3b5be5721b0a9fd3914afabae4.

---
+++ GenEstApp/RunApp.R
@@ -0,0 +1,20 @@
+################################################################################
+#
+#  This script deploys a local version of the GenEst application
+#
+#  version 0.0.0.5 November 2017
+#
+#  Held under GNU GPL v >= 3	
+#
+################################################################################
+
+  # load dependencies
+
+    source("genestfunctions.R")
+    packageLoad()
+
+
+  # run application 
+
+    runApp()
+
