0dc05a4814a81d48dfda04c81a3087ceb9cfb846
DAPPERstats <dapperstats@gmail.com>
2018-02-24 23:28:15 -0800

    pkm_set_size
    fits the full suite of models for all size classes using the same basic inputs as pkm and pkm_set with an additional argument for the size class column. if that argument is NULL it runs as pkm_set singly and notifies user with a message

--- R/searcher_efficiency_functions.R
+++ R/searcher_efficiency_functions.R
@@ -697,5 +697,70 @@ pkm_set_aicc_tab <- function(pk_model_set){
   return(output)
 }
 
+#' Fit all possible searcher efficiency models across all size classes.
+#'
+#' Function inputs generally follow \code{pkm_set} and \code{pkm} but with an 
+#'  additional size column input and calculation of the set of pkm models for
+#'  each of the size classes
+#'
+#' @param pformula Formula for p; an object of class "\code{\link{formula}}"
+#' (or one that can be coerced to that class): a symbolic description of the
+#' model to be fitted. Details of model specification are given under 
+#' 'Details'.
+#'
+#' @param data Dataframe with results from searcher efficiency trials and any
+#' covariates included in \code{pformula} or {kformula} (required).
+#'
+#' @param kformula Formula for k; an object of class "\code{\link{formula}}"
+#' (or one that can be coerced to that class): a symbolic description of the
+#' model to be fitted. Details of model specification are given under 
+#; 'Details'.
+#'
+#' @param obs_cols Vector of names of columns in \code{data} where results 
+#' for each search occasion are stored (optional). If no \code{obs_cols} are 
+#' provided, \code{pkm} uses as \code{obs_cols} all columns with names that 
+#' begin with an \code{'s'} or \code{'S'} and end with a number, e.g., 's1',
+#' 's2', 's3', etc. This option is included as a convenience for the user, 
+#' but care must be taken that other data are not stored in columns with 
+#' names matching that pattern. Alternatively, \code{obs_cols} may be 
+#' entered as a vector of names, like \code{c('s1', 's2', 's3')}, 
+#' \code{paste0('s', 1:3)}, or \code{c('initialSearch', 'anotherSearch', 
+#' 'lastSearch')}.
+#'
+#' @param fixed_k Parameter for user-specified \code{k} value (optional). If a
+#' value is provided, \code{kformula} is ignored and the model is fit under 
+#' the assumption that the \code{k} parameter is fixed and known to be
+#' \code{fix_k}.
+#'
+#' @param init_k Initial value used for \code{k} in the optimization.
+#'
+#' @param sizeclass_col Name of colum in \code{data} where the size classes
+#'  are recorded
+#'
+#' @return \code{pkm_set} returns a list of objects, each of which is a list
+#'  of \code{pkm}" outputs (each corresponding to the fit of a specific model
+#'  within the set of \code{pkm} models fit for the given size class), that is
+#'  of length equal to the total number of size classes
+#'
+pkm_set_size <- function(pformula, kformula = NULL, data, obs_cols = NULL, 
+                         sizeclass_col = NULL, fixed_k = NULL, k_init = 0.7){
 
+  if(length(sizeclass_col) == 0){
+    message("No size class provided, function run as if pkm_set")
+    output <- pkm_set(pformula, kformula, data, obs_cols, fixed_k, k_init)
+    return(output)
+  }
+
+  sizeclass_data <- as.character(data[ , sizeclass_col])
+  sizeclasses <- unique(sizeclass_data)
+  n_sizeclasses <- length(sizeclasses)
+
+  out <- vector("list", n_sizeclasses)
+  names(out) <- sizeclasses
+  for(i in 1:n_sizeclasses){
+    out[[i]] <- pkm_set(pformula, kformula, data, obs_cols, fixed_k, k_init)
+  }
+
+  return(out)
+}
 
