56907d5c37ec18aa086f5eacb66aa1309e9b5eab
DAPPERstats <dapperstats@gmail.com>
2018-02-25 21:37:24 -0800

    moving vignette code to code scraps folder
    given the removal of old functions the full vignette doesnt run right now
    the new vignette still needs to be drafted and the code in that script is not stable

---
+++ code_scraps/command_line_vignette.R
@@ -0,0 +1,34 @@
+SE_data <- read.csv("inst/extdata/ExampleSearcherEfficiency.csv")
+
+sizeclass_col <- "Size"
+
+
+SE_data <- SEdataIn[SEdataIn$Size == "S", ]
+
+pkm_set_s <- pkm_set(data = SE_data, pformula = p ~ HabitatType*Visibility)
+SE_mods <- pkm_set_size(data = SE_data, pformula = p ~ Visibility*HabitatType,
+kformula = k~1, sizeclass_col = "Size")
+
+pkm_check(SE_mods)
+
+pk1 <- pkm(data = SE_data, pformula = p ~ Visibility*Size*HabitatType)
+
+
+pkm_set_aicc_tab(SE_mods[[1]])
+names(SE_mods)[as.numeric(row.names(pkm_set_aicc_tab(SE_mods)))]
+length(SE_mods)
+SE_mods[[1]][[25]]
+
+pkm_set_s[[1]][[1]]
+
+sizeclasses <- unique(SE_data[ , sizeclass_col])
+sizeclass_chosen <- which(sizeclasses == "XL")
+pkm_set_aicc_tab(NULL)
+
+rv$SE_data <- SEdataIn
+rv$fixed_k
+
+
+
+      rv$SE_mod <- pkm(pformula = p ~ 1, kformula = k ~ 1,
+                       data = rv$SE_data, fixed_k = rv$fixed_k)
