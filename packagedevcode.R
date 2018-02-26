03606d800b42eadc2c5f068900a9fc5f936a4b17
DAPPERstats <dapperstats@gmail.com>
2018-02-26 00:36:45 -0800

    bringing ui structure back in
    this now includes all of the tabs and tabsets and such
    however nothing is happening on the survey side behind the CP g or M estimations at this time.
    you can use the data selection drop downs and such as you would but then nothing happens when you push run models
    the only thing that remains to do for the SE analyses is include the figures, which we need to discuss our approach now given the more flexible inputs

--- packagedevcode.R
+++ packagedevcode.R
@@ -1,48 +1,17 @@
-##############################################################################
-#
-# package development code for GenEst
-#
 
+  library(devtools)
 
-  # load devtools
+  pkgloc <- getwd()
 
-    library(devtools)
+  pkgdpns <- c("shiny", "shinythemes", "rhandsontable", "httr", "survival", 
+                "mvtnorm", "matrixStats", "gsl", "DT")
 
-
-  # set the package location
-
-    pkgloc <- getwd()
-
-
-  # Set up files
-
-  #
-  #  NOTE: This code only needs to be run once!
-  #         Currently commented out
-  #
-
-  #  # create the barebones package structure 
-  
-  #    devtools::create(pkgloc)
-
-  #  # create the bare bones vignette
-
-  #    devtools::use_vignette("command-line-example", pkgloc)
-
-
-  # add dependencies to the description and load them here 
-
-    pkgdpns <- c("shiny", "shinythemes", "rhandsontable", "httr", "survival", 
-                 "mvtnorm", "matrixStats", "gsl", "DT")
-
-    for(i in 1:length(pkgdpns)){
-      devtools::use_package(pkgdpns[i], "Imports", pkgloc)
-    }
-
+  for(i in 1:length(pkgdpns)){
+    devtools::use_package(pkgdpns[i], "Imports", pkgloc)
+  }
 
   devtools::load_all(devtools::as.package(pkgloc))
   devtools::document(devtools::as.package(pkgloc))
-
 
   devtools::build_vignettes(pkgloc)
   devtools::build(pkgloc, binary = T)
