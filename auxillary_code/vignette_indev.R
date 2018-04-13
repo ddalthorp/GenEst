c48787cd4aeb6652a833ec46ee5eb61fad3b2bd2
DAPPERstats <dapperstats@gmail.com>
2018-04-13 02:24:47 -0700

    improve error handling for SE in app
    base level pkm errors are now passed all the way up to the gui and dont (shouldnt) cause breaks in the app

--- auxillary_code/vignette_indev.R
+++ auxillary_code/vignette_indev.R
@@ -7,19 +7,22 @@ data_SE <- mockData$SearcherEfficiencyData
 
 
 
-xx <- GenEst::pkm(formula_p = p ~ 1, formula_k = k~1, data = data_SE, 
-        obsCol = c("Search1"), kFixed = 0.2, kInit = 0.7, CL = 0.9)
+xx <-
+gg<-tryCatch( GenEst::pkm(formula_p = p ~ 1, formula_k = k~1, data = data_SE, 
+        obsCol = c("Search4", "Search3", "Search2", "Search1"), kFixed = 0.2, kInit = 0.7, CL = 0.9),
+      error = function(x){geterrmessage()})
 
 
 devtools::load_all()
 
-xx <- GenEst::pkmSet(formula_p = p ~ Visibility, formula_k = k~Visibility, 
+xcx <- pkmSet(formula_p = p ~ Visbility, formula_k = k~1, 
         data = data_SE, 
-        obsCol = c("Search1"), kFixed = 0.5, kInit = 0.7, CL = 0.9)
+        obsCol = c("Search4", "Search3", "Search2", "Search1"), kFixed = 0.5, kInit = 0.7, CL = 0.9)
 
 xx <- GenEst::pkmSetSize(formula_p = p ~ Visibility, formula_k = k~1, 
-        data = data_SE, sizeclassCol = NULL, 
-        obsCol = c("Search1"), kFixed = NULL, kInit = 0.7, CL = 0.9)
+        data = data_SE, sizeclassCol = "Size", 
+        obsCol = c("Search4", "Search3", "Search2", "Search1"), 
+        kFixed = NULL, kInit = 0.7, CL = 0.9)
 
 
 
