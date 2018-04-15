a41f056f4ea924707677537f6a66eca74dcb0988
DAPPERstats <dapperstats@gmail.com>
2018-04-14 19:47:49 -0700

    full draft vignette through the se and cp analyses
    also minor edits to figure functions to provide better default output

--- auxillary_code/command_line_vignette-cp.R
+++ auxillary_code/command_line_vignette-cp.R
@@ -7,8 +7,8 @@ right <- "FirstAbsentDecimalDays"
 
 data <- data_CP
 
-formula_l <- l ~ Visibility*Season
-formula_s <- s ~ Visibility*Season
+formula_l <- l ~ Visibility
+formula_s <- s ~ Season
 dist <- "exponential"
 
 x<-cpm(formula_l, formula_s, data, left, right, dist = dist)
