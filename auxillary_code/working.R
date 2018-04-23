760ff35d39f086f5952e7bb1cd8391105f0fcac7
DAPPERstats <dapperstats@gmail.com>
2018-04-23 01:40:16 -0700

    full integration of the ghat code across flexible size class inputs
    also now include a function yyyymmdd for converting dates from common excel format to standard r date

--- auxillary_code/working.R
+++ auxillary_code/working.R
@@ -12,13 +12,38 @@ cpModel <- GenEst::cpm(formula_l = l ~ Season, formula_s = s ~ 1, data = dataCP,
              right = "FirstAbsentDecimalDays", dist = "weibull")
 
 
+ds <- data_ss[,"DateSearched"]
+ds <- format(ds, format = "%m/%d/%Y")
+data_ss[,"DateSearched"] <- ds
+
 data_carc <- mockData$CarcassObservationData
 data_ss <- mockData$SearchScheduleData
 SEmod <- pkModel
 CPmod <- cpModel
 
 
-rghat(n = 1000, data_carc, data_ss, model_SE = pkModel, model_CP = cpModel, 
+rghat(n = 10, data_carc, data_ss, model_SE = pkModel, model_CP = cpModel, 
 seed_SE = 1, seed_CP = 1, kFill = NULL, unitCol = "Unit",
                   dateFoundCol = "DateFound", 
 dateSearchedCol = "DateSearched")
+
+
+
+pkmModSetSize <- pkmSetSize(formula_p = p ~ Visibility*HabitatType, 
+                   formula_k = k ~ HabitatType, data = dataSE,
+                   obsCol = c("Search1", "Search2", "Search3", "Search4"),
+                   sizeclassCol = "Size"
+                 )
+cpmModSetSize <- cpmSetSize(formula_l = l ~ Visibility*Season, 
+                   formula_s = s ~ Visibility, data = dataCP,
+                   left = "LastPresentDecimalDays", 
+                   right = "FirstAbsentDecimalDays", 
+                   dist = c("exponential", "lognormal"),
+                   sizeclassCol = "Size"
+                 )
+
+
+
+
+
+
