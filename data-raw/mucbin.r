00d878be69d703669c5c300e3cab372eafc755aa
ddalthorp <ddalthorp@usgs.gov>
2018-04-27 07:26:11 -0700

    restore robustness for extreme tails
    improved accuracy in tails

--- data-raw/mucbin.r
+++ data-raw/mucbin.r
@@ -1,25 +1,30 @@
-# This script defines the mucbin function, which uses linear interpolation to
-# calculate the mean of the continuous binomial distribution with parameters
-# size = 1/prob.
+# This script creates the data used by the mucbinom function
 eps <- .Machine$double.eps
-g <- c(0.0001261, 0.99999)
+g <- c(0.0001205, 1 - sqrt(eps))
 mug <- numeric(2)
 for (gi in 1:length(g)){
-  mug[gi] <- integrate(function(x, size, prob) 1 - cbinom::pcbinom(x, size, prob),
-  size = 1/g[gi], prob = g[gi], lower = 0, upper = 1/g[gi] + 1 - eps)$val
-}
-y <- seq(min(mug), max(mug), length = 200) # 0 and 1 will be appended at the end
-gkey <- numeric(length(y))
-for (yi in 1:length(y)){
-  gkey[yi] <- uniroot(f = function(prob){
-    integrate(function(x) 1 - cbinom::pcbinom(x, 1/prob, prob),
-      lower = 0, upper = 1/prob + 1)$val - y[yi]
+  mug[gi] <- integrate(
+    function(x, size, prob){
+      1 - exp(cbinom::pcbinom(x, size, prob, log = T))
     },
-    lower = 0.00012609,
-    upper = 1 - eps)$root
+    size = 1/g[gi], prob = g[gi],
+    lower = 0, upper = 1/g[gi] + 1 - 2 * eps, rel.tol = sqrt(eps)
+  )$val
 }
-gkey <- c(0, gkey, 1)
-Ecbin <- c(1.48204, y, 2)
-mucbin <- approxfun(x = gkey, y = Ecbin)
-devtools::use_data(mucbin, internal = T)
+mug
+Ecbin <- seq(min(mug), max(mug), length = 200)
+gkey <- numeric(length(Ecbin))
+for (yi in 1:length(Ecbin)){
+  gkey[yi] <- uniroot(f = function(prob){
+    integrate(function(x) 1 - exp(cbinom::pcbinom(x, 1/prob, prob, log = T)),
+      lower = 0, upper = 1/prob + 1)$val - Ecbin[yi]
+    },
+    lower = 0,
+    upper = 1)$root
+}
 
+gkey <- c(0, gkey, 1)
+Ecbin <- c(1.481, Ecbin, 2)
+mucbinomxy <- list("x" = gkey, "y" = Ecbin)
+
+devtools::use_data(mucbinomxy, internal = TRUE)
