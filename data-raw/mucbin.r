400f73066b31b8f995445a4e45b5c79d1316372f
ddalthorp <ddalthorp@usgs.gov>
2018-04-27 11:01:33 -0700

    restore name, improve accuracy and robustness

--- data-raw/mucbin.r
+++ data-raw/mucbin.r
@@ -1,6 +1,6 @@
 # This script creates the data used by the mucbinom function
 eps <- .Machine$double.eps
-g <- c(0.0001205, 1 - sqrt(eps))
+g <- c(0.0001205, 1 - 2*eps)
 mug <- numeric(2)
 for (gi in 1:length(g)){
   mug[gi] <- integrate(
@@ -8,7 +8,8 @@ for (gi in 1:length(g)){
       1 - exp(cbinom::pcbinom(x, size, prob, log = T))
     },
     size = 1/g[gi], prob = g[gi],
-    lower = 0, upper = 1/g[gi] + 1 - 2 * eps, rel.tol = sqrt(eps)
+    lower = 0, upper = 1/g[gi] + 1 - 2*eps,
+    rel.tol = 100*eps
   )$val
 }
 mug
@@ -16,11 +17,13 @@ Ecbin <- seq(min(mug), max(mug), length = 200)
 gkey <- numeric(length(Ecbin))
 for (yi in 1:length(Ecbin)){
   gkey[yi] <- uniroot(f = function(prob){
-    integrate(function(x) 1 - exp(cbinom::pcbinom(x, 1/prob, prob, log = T)),
-      lower = 0, upper = 1/prob + 1)$val - Ecbin[yi]
+      integrate(function(x) 1 - exp(cbinom::pcbinom(x, 1/prob, prob, log = T)),
+        rel.tol = 100*eps,
+        lower = 0, upper = 1/prob + 1
+      )$val - Ecbin[yi]
     },
-    lower = 0,
-    upper = 1)$root
+    lower = 0,  upper = 1,
+    tol = 10*eps)$root
 }
 
 gkey <- c(0, gkey, 1)
