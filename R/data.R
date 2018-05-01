65bc5a901e6e6096a8d53a06bd7081506409de69
DAPPERstats <dapperstats@gmail.com>
2018-05-01 02:08:59 -0700

    tidying associated with the split and dataset integration
    splits file renamed splits_functions as the others
    name changes in the splits functions to align with existing codebase
    COdat to data_CO
    SSdat to data_SS
    CI_level to CL
    data added by dan named dataset2 and given its own package contents

--- R/data.R
+++ R/data.R
@@ -13,3 +13,20 @@
 #' }
 #' @source Mock
 "mockData"
+
+#' Description of the dataset
+#'
+#' A dataset containing SE, CP, SS, DWP, and CO data.
+#'
+#' @format A list with 7 items:
+#' \describe{
+#'   \item{SE_trials}{Searcher efficiency trial data}
+#'   \item{CP_trials}{Carcass persistence trial data}
+#'   \item{SS}{Search schedule data}
+#'   \item{DWP_All60}{Density weighted proportion of area searched data}
+#'   \item{DWP_RP120}{Density weighted proportion of area searched data}
+#'   \item{CO_dataAll60}{Carcass observations}  
+#'   \item{CO_dataRP120}{Carcass observations}  
+#' }
+#' @source Source name
+"dataset2"
