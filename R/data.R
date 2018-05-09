0c3cd6562da5e0902f2bdf81e0b5b14faba6d258
DAPPERstats <dapperstats@gmail.com>
2018-05-09 10:57:32 -0700

    edits to Aj, time, and data sets
    Aj matrix now includes row names that are the unit names where the carcass was found
    time now is set up with the first observation being at t = 0 not t = 1
    updated documentation for the new data sets
    also added elipsis argument to the splits function to make sure every argument included is in the documentation

--- R/data.R
+++ R/data.R
@@ -18,15 +18,29 @@
 #'
 #' A dataset containing SE, CP, SS, DWP, and CO data.
 #'
-#' @format A list with 7 items:
+#' @format A list with 5 items:
 #' \describe{
 #'   \item{SE_trials}{Searcher efficiency trial data}
 #'   \item{CP_trials}{Carcass persistence trial data}
-#'   \item{SS}{Search schedule data}
-#'   \item{DWP_All60}{Density weighted proportion of area searched data}
+#'   \item{SS_23cleared60}{Search schedule data}
+#'   \item{DWP_23cleared60}{Density weighted proportion of area searched data}
+#'   \item{CO_23cleared60}{Carcass observations}  
+#' }
+#' @source Source name
+"windClearedPlotData"
+
+#' Description of the dataset
+#'
+#' A dataset containing SE, CP, SS, DWP, and CO data.
+#'
+#' @format A list with 5 items:
+#' \describe{
+#'   \item{SE_trials}{Searcher efficiency trial data}
+#'   \item{CP_trials}{Carcass persistence trial data}
+#'   \item{SS_RP120}{Search schedule data}
 #'   \item{DWP_RP120}{Density weighted proportion of area searched data}
-#'   \item{CO_dataAll60}{Carcass observations}  
 #'   \item{CO_dataRP120}{Carcass observations}  
 #' }
 #' @source Source name
-"dataset2"
+"windRPData"
+
