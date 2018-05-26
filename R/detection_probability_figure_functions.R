2d577f965f3071dbe1af8c220386a4931308024e
DAPPERstats <dapperstats@gmail.com>
2018-05-26 04:34:48 -0700

    addressing issues with multi-size class generic g plotting
    in the big vignette (now updated to include generic g plot for real) and in the app

--- R/detection_probability_figure_functions.R
+++ R/detection_probability_figure_functions.R
@@ -68,7 +68,7 @@ plot.gGeneric <- function(x, sizeclassName = NULL, CL = 0.9, ...){
   }
 
   par(mar = c(2,0,2,0))
-  par(fig = c(0.9, 1, 0, 1), new = TRUE)
+  par(fig = c(0.9, 1, 0.25, 0.75), new = TRUE)
   plot(1,1, type = "n", bty = "n", xaxt = "n", yaxt = "n", xlab = "", 
     ylab = "", ylim = c(0, 1), xlim = c(0, 1)
   )
@@ -106,15 +106,102 @@ plot.gGenericSize <- function(x, CL = 0.9, ...){
   nsizeclass <- length(x)
 
   if (nsizeclass == 1){
-    devAskNewPage(FALSE)
-  }else{
-    devAskNewPage(TRUE)
+    return(plot(x[[1]], CL = CL, ...))
   }
 
   sizeclassNames <- names(x)
+
+  increm <- 1 / nsizeclass
+  y1 <- seq(0, 1, by = increm)
+  y1 <- y1[-length(y1)]
+  y2 <- seq(0, 1, by = increm)
+  y2 <- y2[-1]
+  par(fig = c(0, 1, 0, 1))
+  par(mar = c(3, 3, 1, 1))  
+  plot(1, 1, type = "n", ylab= "", xlab = "", xaxt = "n", yaxt = "n", 
+    bty = "n"
+  )  
+  mtext(side = 2, line = 2, "Detection Probability", cex = 1)
+
   for (sci in 1:nsizeclass){
-    plot(x[[sci]], sizeclassName = sizeclassNames[sci], CL = CL, ...)
+
+    par(fig = c(0, 0.9, y1[sci], y2[sci]), new = TRUE)
+    gs <- x[[sci]]$g
+    cells <- names(gs)
+    ncell <- length(cells)
+    predsByCell <- strsplit(cells, "\\.")
+    npred <- length(predsByCell[[1]])
+    colNames <- colnames(x[[sci]]$predictors)
+    predNames <- colNames[-grep("CellNames", colNames)]
+    labelText <- paste("Labels = ", paste(predNames, collapse = "."))
+
+    par(mar = c(2, 4, 2, 1))  
+    plot (1, 1, type = "n", xlab = "", ylab = "", xaxt = "n", yaxt = "n", 
+      bty = "L", xlim = c(0.5, ncell + 0.5), ylim = c(0, 1)
+    )
+    axis(2, at = seq(0, 1, 0.25), las = 1, cex.axis = 0.5)
+    axis(1, at = 1:ncell, labels = FALSE, tck = -0.05)
+    ang <- 0
+    offx <- NULL
+    if (ncell > 3){
+      ang <- 45 
+      offx <- 1
+    }
+
+    text(1:ncell, -0.2, srt = ang, adj = offx, labels = cells, xpd = TRUE, 
+      cex = 0.75
+    )
+    if (predNames[1] == "group" & length(predNames) == 1 & cells[1] == "all"){
+      labelText <- NULL
+    }
+
+    text(0.5, 1.15, labelText, xpd = TRUE, cex = 0.5, adj = 0)
+
+    if (!is.null(sizeclassNames[sci])){
+      text_sc <- paste("Size class: ", sizeclassNames[sci], sep = "")
+      text(x = 0.5, y = 1.25, xpd = TRUE, text_sc, adj = 0, cex = 0.5)
+    }
+
+    probs <- c(0, (1 - CL) / 2, 0.25, 0.5, 0.75, 1 - (1 - CL) / 2, 1)
+
+    for (celli in 1:ncell){
+      xx <- celli
+      yy <- quantile(gs[[celli]], probs)
+
+      med <- c(-0.1, 0.1)
+      tb <- c(-0.07, 0.07)
+
+      rect(xx - 0.1, yy[3], xx + 0.1, yy[5], lwd = 2, border = 1) 
+      points(xx + med, rep(yy[4], 2), type = 'l', lwd = 2, col = 1)
+      points(xx + tb, rep(yy[2], 2), type = 'l', lwd = 2, col = 1)
+      points(xx + tb, rep(yy[6], 2), type = 'l', lwd = 2, col = 1)
+      points(rep(xx, 3), yy[1:3], type = 'l', lwd = 2, col = 1)
+      points(rep(xx, 3), yy[5:7], type = 'l', lwd = 2, col = 1)
+
+    }
   }
 
-  devAskNewPage(FALSE)
+  par(mar = c(2,0,2,0))
+  par(fig = c(0.9, 1, 0, 1), new = TRUE)
+  plot(1,1, type = "n", bty = "n", xaxt = "n", yaxt = "n", xlab = "", 
+    ylab = "", ylim = c(0, 1), xlim = c(0, 1)
+  )
+  x_s <- 0.1
+  CL_split <- (1 - CL) / 2
+  probs_y <- c(0, CL_split, 0.25, 0.5, 0.75, 1 - CL_split, 1)
+  set.seed(12)
+  y_s <- quantile(rnorm(1000, 0.5, 0.1), probs = probs_y)
+  med <- c(-0.1, 0.1)
+  tb <- c(-0.07, 0.07)
+  rect(x_s - 0.1, y_s[3], x_s + 0.1, y_s[5], lwd = 2, border = 1) 
+  points(x_s + med, rep(y_s[4], 2), type = 'l', lwd = 2, col = 1)
+  points(x_s + tb, rep(y_s[2], 2), type = 'l', lwd = 2, col = 1)
+  points(x_s + tb, rep(y_s[6], 2), type = 'l', lwd = 2, col = 1)
+  points(rep(x_s, 3), y_s[1:3], type = 'l', lwd = 2, col = 1)
+  points(rep(x_s, 3), y_s[5:7], type = 'l', lwd = 2, col = 1)
+  num_CL <- c(CL_split, 1 - CL_split) * 100
+  text_CL <- paste(num_CL, "%", sep = "")
+  text_ex <- c("min", text_CL[1], "25%", "50%", "75%", text_CL[2], "max")
+  text(x_s + 0.2, y_s, text_ex, cex = 0.65, adj = 0)
+
 }
