1ae702019a3980dc33e41fb3dd68b1b27c4732f8
Kevin VanNess <75155102+Kevanness@users.noreply.github.com>
2023-05-25 01:18:36 -0400

    Update docs
    Update documentation files.

--- src/RcppExports.cpp
+++ src/RcppExports.cpp
@@ -5,6 +5,11 @@
 
 using namespace Rcpp;
 
+#ifdef RCPP_USE_GLOBAL_ROSTREAM
+Rcpp::Rostream<true>&  Rcpp::Rcout = Rcpp::Rcpp_cout_get();
+Rcpp::Rostream<false>& Rcpp::Rcerr = Rcpp::Rcpp_cerr_get();
+#endif
+
 // calcRateC
 NumericMatrix calcRateC(NumericMatrix M, NumericMatrix Aj, NumericVector days, NumericMatrix searches);
 RcppExport SEXP _GenEst_calcRateC(SEXP MSEXP, SEXP AjSEXP, SEXP daysSEXP, SEXP searchesSEXP) {
